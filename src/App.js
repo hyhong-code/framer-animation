f641bec41dbcc6adb156a2ec9e4a10dcccfee5c5
Haiyang Hong <hong961127@gmail.com>
2020-10-12 21:32:19 -0700

    Explore transition with state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import { Card, CardGrid, Container, Header } from "./Elements";
-import "./App.css";
+
 import Menu from "./Menu";
 import blue from "./blue.png";
 import purp from "./purp.png";
 import black from "./black.png";
 import green from "./green.png";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [value, setValue] = useState(0);
+  const [isToggled, setToggle] = useState(1);
+
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <Header>
         <Menu />
         <h1>Header</h1>
       </Header>
       <Container>
-        <h2>Super Cool</h2>
+        <motion.h2 animate={{ x: Number(value) * 5, opacity: isToggled }}>
+          Super Cool
+        </motion.h2>
+        <button onClick={() => setToggle((prev) => (prev === 1 ? 0 : 1))}>
+          Toggle
+        </button>
+        <input
+          type="range"
+          min="-100"
+          max="100"
+          value={value}
+          onChange={(evt) => {
+            setValue(evt.target.value);
+          }}
+        />
         <CardGrid>
           <Card style={{ background: "var(--purp)" }}>
             <h3>Some card</h3>
@@ -35,8 +59,8 @@ function App() {
           </Card>
         </CardGrid>
       </Container>
-    </div>
+    </motion.div>
   );
-}
+};
 
 export default App;
